67c3283d07dcd90bd91ed47110a7b637230fbc56
Charmmmy <2127137114@qq.com>
2025-11-19 23:40:43 +0800

    Add files via upload

---
+++ &useful/&useful/&useful.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+int main()
+{
+	int age = 21,i;//00010101
+	for(i=0;i<8;i++)\
+	{
+		if (age & 0x1)//按位与运算
+		{
+			printf("1");
+		}
+		else
+		{
+			printf("0");
+		}
+		age >>= 1;//》右移符
+	}
+
+}//10101000turn
---
+++ zhengxiang/zhengxiang/zhengxiang.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+int main()
+{
+	int age = 21, i;//00010101
+	for (i = 0; i < 8; i++)\
+	{
+		if (age & 0x80)//按位与运算与10000000比较可得正序
+		{
+			printf("1");
+		}
+		else
+		{
+			printf("0");
+		}
+		age <<= 1;//》右移符
+	}
+
+}//10101000turn
